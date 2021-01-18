e1fffeb34ddec1d83a3df4776d3e5bd302103f39
Peyton Schlafley <pschlafley0@gmail.com>
2021-01-18 18:24:39 -0500

    initial files and started creating tests for our actions and reducers

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,24 @@
+import { useReducer } from 'react';
+import {
+    UPDATE_CATEGORIES,
+    UPDATE_PRODUCTS,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+
+// create a sample of what our global state will look like
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+};
+
+test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products: [{}, {}]
+    });
+
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
